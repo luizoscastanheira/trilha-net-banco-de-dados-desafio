5cef982e7f3a9181ed6b3122afec8b1d3d29d4c5
Luiz Otávio - ADS <luizoscastanheira@hotmail.com.br>
2025-08-12 15:37:49 -0300

    Create SQLQueryRespostaDesafio.sql
    Criando arquivo de sql.

---
+++ SQLQueryRespostaDesafio.sql
@@ -0,0 +1,31 @@
+-- 1 Buscar o nome e ano dos filmes
+SELECT Nome, Ano FROM Filmes;
+
+-- 2 Buscar o nome e ano dos filmes ordenados por ordem crescente pelo ano
+SELECT Nome, Ano FROM Filmes ORDER BY Ano;
+
+-- 3 Buscar pelo filme De Volta para o Futuro, Trazendo o nome, ano e a duração
+SELECT * FROM Filmes WHERE Nome LIKE '%Futuro%';
+
+-- 4 Buscar os Filmes lançados em 1997
+SELECT * FROM Filmes WHERE Ano = 1997;
+
+-- 5 Buscar os filmes lançados após o ano 2000
+SELECT * FROM Filmes WHERE Ano > 2000;
+
+-- 6 Buscar os filmes com duração maior que 100 e menor que 150,ordenando pela duração em ordem crescente
+SELECT * FROM Filmes WHERE Duracao > 100 AND Duracao < 150 ORDER BY Duracao;
+
+-- 7 Buscar a quantidade de filmes lançados no ano, agrupando por ano, ordenando pela duraçao em ordem decrescente
+-- OBS Creio haver um erro no enunciado e a ordenção corrreta seja por Quantidade, como feito abaixo
+SELECT Ano, COUNT(Ano) Quantidade FROM Filmes GROUP BY Ano ORDER BY Quantidade DESC;
+
+-- 8 Buscar os Atores do gênero Masculino, Retornando o PrimeiroNome, UltimoNome
+SELECT * FROM Atores WHERE Genero ='M';
+
+-- 9 Buscar os Atores do gênero feminino, retornando o PrimeiroNome, UltimoNome e ordenando pelo PrimeiroNome
+SELECT * FROM Atores WHERE Genero ='F' ORDER BY PrimeiroNome;
+
+-- 10 Buscar o nome do Filme e o Gênero -- AInda fazendo
+SELECT * FROM Filmes;
+SELECT * FROM FilmesGenero;
